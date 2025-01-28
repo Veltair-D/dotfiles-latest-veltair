9290f57277fb007f9164b56d01fea9ca00d79f19
Veltair-D <fabian.quevedo@upch.pe>
2025-01-28 18:49:41 -0500

    Fixed sketchybar

--- sketchybar/felixkratz-linkarzu/plugins/battery.sh
+++ sketchybar/felixkratz-linkarzu/plugins/battery.sh
@@ -1,22 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
+source "$CONFIG_DIR/colors.sh"
+
+BATTERY_INFO="$(pmset -g batt)"
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
+
+# Check for the absence of "InternalBattery" to determine no battery present
+# My mac mini doesn't have a battery, and it was leaving a blank space where the
+# battery was supposed to be
+if ! echo "$BATTERY_INFO" | grep -q "InternalBattery"; then
+  sketchybar --set "$NAME" drawing=off
+else
+  sketchybar --set "$NAME" drawing=on
+fi
 
 if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
+COLOR=$WHITE
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+9[0-9] | 100)
+  ICON=""
   ;;
-  [6-8][0-9]) ICON=""
+[3-7][0-9])
+  ICON=""
+  COLOR=$YELLOW
   ;;
-  [3-5][0-9]) ICON=""
+# [3-5][0-9])
+#   ICON=""
+#   COLOR=$YELLOW
+#   ;;
+[1-2][0-9])
+  ICON=""
+  COLOR=$RED
   ;;
-  [1-2][0-9]) ICON=""
-  ;;
-  *) ICON=""
+*) ICON="" ;;
 esac
 
 if [[ "$CHARGING" != "" ]]; then
@@ -25,4 +45,4 @@ fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}" icon.color=$COLOR
--- sketchybar/felixkratz-linkarzu/plugins/calendar.sh
+++ sketchybar/felixkratz-linkarzu/plugins/calendar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # If you cant to show the time
-# sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')"
+sketchybar --set $NAME icon="$(date '+%H:%M %a %d. %b ')"
 
 # In case you don't want to show the time
-sketchybar --set $NAME icon="$(date '+%a %d. %b %Y')"
+# sketchybar --set $NAME icon="$(date '+%a %d. %b %Y')"
